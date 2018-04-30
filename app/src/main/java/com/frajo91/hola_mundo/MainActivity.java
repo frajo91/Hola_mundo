d1d313cc9071dd862fce959616e81cb65aa196ba
frajo91 <frajo91@hotmail.com>
2018-04-29 22:19:48 -0500

    version inicial

---
+++ app/src/main/java/com/frajo91/hola_mundo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.frajo91.hola_mundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
